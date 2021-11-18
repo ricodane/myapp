5d50b617054a46c446276ea689dbf5a67f46935e
ricodane <ricodanecatacutan@gmail.com>
2021-11-19 03:48:28 +0800

    First

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,12 +1,60 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { useIsFocused } from "@react-navigation/native";
 
 const CameraScreen = () => {
+	const [hasPermission, setHasPermission] = useState(null);
+	const [type, setType] = useState(Camera.Constants.Type.back);
+	const isFocused = useIsFocused();
+
+	useEffect(() => {
+		(async () => {
+			const { status } = await Camera.requestCameraPermissionsAsync();
+			setHasPermission(status === "granted");
+		})();
+	}, []);
+
+	if (hasPermission === null) {
+		return <View />;
+	}
+	if (hasPermission === false) {
+		return alert("You have not granted permission to open Camera.");
+	}
+
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>This is camera</Text>
-		</View>
+		isFocused && (
+			<Camera style={{ flex: 1 }} type={type}>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: "transparent",
+						flexDirection: "row",
+					}}
+				>
+					<TouchableOpacity
+						style={{ flex: 0.1, alignSelf: "flex-end", alignItems: "center" }}
+						onPress={() => {
+							setType(
+								type === Camera.Constants.Type.back
+									? Camera.Constants.Type.front
+									: Camera.Constants.Type.back
+							);
+						}}
+					>
+						<Text style={styles.text}> Flip </Text>
+					</TouchableOpacity>
+				</View>
+			</Camera>
+		)
 	);
 };
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 18,
+		color: "white",
+	},
+});
 
 export default CameraScreen;
--- screens/GalleryScreen.js
+++ screens/GalleryScreen.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import { launchImageLibrary } from "react-native-image-picker";
+import { Button } from "react-native-paper";
+import ImagePicker from "react-native-image-crop-picker";
+
+const [image, setImage] = useState("");
+
+const uploadImage = () => {
+	ImagePicker.openPicker({
+		width: 300,
+		height: 400,
+		cropping: true,
+	}).then((image) => {
+		console.log(image);
+	});
+};
+
+launchImageLibrary(options, (response) => {
+	if (response.didCancel) {
+		Alert.alert("User cancelled access to gallery");
+	} else if (response.errorCode == "permission") {
+		Alert.alert("permission not satisfied");
+	} else if (response.errorCode == "others") {
+		console.log("others");
+	} else if (response.assets[0].fileSize > 2097152) {
+		Alert.alert("Maximum image size exceed", "Please choose another image", [
+			{ text: "OK" },
+		]);
+	}
+});
 
 const GalleryScreen = () => {
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>This is gallery</Text>
+		<View>
+			<TouchableOpacity onPress={() => uploadImage()}>
+				<Text>Upload Image</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
